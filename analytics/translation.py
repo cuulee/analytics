26809474ae22ca675522d332fa7de0f51f5e61b2
npmiller <nicolas.pierre.miller@gmail.com>
2014-11-24 10:43:30 +0100

    Add modeltranslation support to analyses

---
+++ analytics/translation.py
@@ -0,0 +1,19 @@
+from modeltranslation.translator import TranslationOptions
+from analytics.models import Analysis, ChartTip
+from geonode.base.models import ResourceBase
+# We need to retrieve the translator after ResourceBase was added to it
+from geonode.base.translation import translator
+
+class AnalysisTranslationOptions(TranslationOptions):
+    fields = (
+        'title',
+        'abstract',
+    )
+
+class ChartTipTranslationOptions(TranslationOptions):
+    fields = (
+        'message',
+    )
+
+translator.register(Analysis, AnalysisTranslationOptions)
+translator.register(ChartTip, ChartTipTranslationOptions)
